64de6dc7184f5b768b4185cc931f9e1483bbd93e
Allen Cui <allen_st_clair@msn.com>
2021-04-07 11:46:34 +0800

    Update NetplanCopy.sh

--- NetplanCopyForUbuntu/NetplanCopy.sh
+++ NetplanCopyForUbuntu/NetplanCopy.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 if test -f "/boot/firmware/netplan.yaml"; then
    rm -fr /etc/netplan/*
-   cp /boot/firmware/network-config /etc/netplan/50-cloud-init.yaml
+   cp /boot/firmware/netplan.yaml /etc/netplan/50-cloud-init.yaml
    netplan apply
+   rm /boot/firmware/netplan.yaml
 fi
