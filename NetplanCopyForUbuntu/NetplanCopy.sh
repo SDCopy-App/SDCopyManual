1e196365be5a981a3ad35320f596b43c88684054
Allen Cui <allen_st_clair@msn.com>
2021-04-22 17:39:16 +0800

    update netplan copy

--- NetplanCopyForUbuntu/NetplanCopy.sh
+++ NetplanCopyForUbuntu/NetplanCopy.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
-if test -f "/boot/firmware/netplan.yaml"; then
+if [ -f "/boot/firmware/netplan.yaml" ]; then
    rm -fr /etc/netplan/*
    cp /boot/firmware/netplan.yaml /etc/netplan/50-cloud-init.yaml
    netplan apply
    rm /boot/firmware/netplan.yaml
 fi
+exit 0
